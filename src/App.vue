85951314f708448ef7d215292c854120c6b01fc7
Kirill <kupuk132@gmail.com>
2021-05-14 09:45:00 +0300

    add graph component

--- src/App.vue
+++ src/App.vue
@@ -26,8 +26,13 @@
       </svg>
     </div>
     <div v-else class="container">
-
-      <add-ticker @view-autocomlete='sortHelpTickers' :helps='helps' :validTickerName='validTickerName' @add-ticker='add' :disabled='tooManyTickersAdded'/>
+      <add-ticker
+        @view-autocomlete="sortHelpTickers"
+        :helps="helps"
+        :validTickerName="validTickerName"
+        @add-ticker="add"
+        :disabled="tooManyTickersAdded"
+      />
 
       <div v-if="tickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
@@ -92,46 +97,13 @@
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </div>
-      <section class="relative" v-if="selectedTicker">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedTicker.name }}
-        </h3>
-        <div class="flex items-end border-gray-600 border-b border-l h-64" ref="graph">
-          <div
-            v-for="(bar, i) in normalizedGraph"
-            :key="i"
-            :style="{ height: `${bar}%` }"
-            class="bg-purple-800 border w-10"
-          ></div>
-        </div>
-        <button
-          @click="selectedTicker = null"
-          type="button"
-          class="absolute top-0 right-0"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            xmlns:svgjs="http://svgjs.com/svgjs"
-            version="1.1"
-            width="30"
-            height="30"
-            x="0"
-            y="0"
-            viewBox="0 0 511.76 511.76"
-            style="enable-background:new 0 0 512 512"
-            xml:space="preserve"
-          >
-            <g>
-              <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                fill="#718096"
-                data-original="#000000"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </section>
+      <graph
+        v-if="selectedTicker"
+        :selectedTicker="selectedTicker"
+        @clear-selected-ticker="selectedTicker = null"
+        @max-graph-elements='selectedTicker'
+        :graph='graph'
+      />
     </div>
   </div>
 </template>
@@ -143,11 +115,13 @@ import {
   unsubscribeFromTicker,
 } from "./api";
 
-import AddTicker from './components/AddTicker'
+import AddTicker from "./components/AddTicker";
+import Graph from "./components/Graph.vue";
 
 export default {
   components: {
     AddTicker,
+    Graph,
   },
 
   name: "App",
@@ -160,7 +134,6 @@ export default {
       selectedTicker: null,
 
       graph: [],
-      maxGraphElements: null,
 
       page: 1,
 
@@ -200,17 +173,9 @@ export default {
     this.loader = false;
   },
 
-  mounted() {
-    window.addEventListener('resize', this.calculateMaxGraphElements)
-  },
-
-  beforeOnmount() {
-    window.removeEventListener('resize', this.calculateMaxGraphElements)
-  },
-
   computed: {
     tooManyTickersAdded() {
-      return this.tickers.length > 4
+      return this.tickers.length > 4;
     },
 
     startIndex() {
@@ -233,19 +198,6 @@ export default {
       return this.filteredTickers.length > this.endIndex;
     },
 
-    normalizedGraph() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
-
-      if (maxValue === minValue) {
-        return this.graph.map(() => 50);
-      }
-
-      return this.graph.map(
-        (e) => 5 + ((e - minValue) * 95) / (maxValue - minValue)
-      );
-    },
-
     pageStateOptions() {
       return {
         filter: this.filter,
@@ -254,20 +206,13 @@ export default {
     },
   },
   methods: {
-    calculateMaxGraphElements() {
-      if(!this.$refs.graph) {
-        return
-      }
-      this.maxGraphElements = this.$refs.graph.clientWidth / 38;
-    },
-
     updateTicker(tickerName, price) {
       this.tickers
         .filter((t) => t.name === tickerName)
         .forEach((t) => {
           if (t === this.selectedTicker) {
             this.graph.push(price);
-            while(this.graph.length > this.maxGraphElements) {
+            while (this.graph.length > this.maxGraphElements) {
               this.graph.shift();
             }
           }
@@ -283,9 +228,7 @@ export default {
       for (const [key, value] of Object.entries(this.validationData)) {
         if (
           key.toLowerCase().startsWith(evt.toLowerCase()) ||
-          value.FullName.toLowerCase().startsWith(
-            evt.toLowerCase()
-          ) ||
+          value.FullName.toLowerCase().startsWith(evt.toLowerCase()) ||
           value.Symbol.toLowerCase().startsWith(evt.toLowerCase())
         ) {
           helpTickerKeys.push(key);
@@ -322,7 +265,7 @@ export default {
 
       const currentTicker = {
         name: ticker,
-        price: '-',
+        price: "-",
       };
 
       this.tickers = [...this.tickers, currentTicker];
@@ -345,16 +288,15 @@ export default {
       }
       unsubscribeFromTicker(t.name);
     },
-
   },
   watch: {
     tickers() {
       localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
     },
 
-    selectedTicker() {
+    selectedTicker(e) {
       this.graph = [];
-      this.$nextTick().then(this.calculateMaxGraphElements())
+      this.$nextTick().then(e);
     },
 
     paginatedTickers() {
